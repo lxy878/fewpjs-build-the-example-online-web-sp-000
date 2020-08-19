5353000b7efc247677242b2b84656b7ac84d3510
Xiaoyi Liu <lxy878@gmail.com>
2020-08-19 11:04:05 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
       // error message
       // remove hidden
       // setTimeout to hide
-      event = mimicServerCall().then(resp => const.log(resp));
+      event = mimicServerCall()
     })
   }
 })
