d585820d431815831ce21970cb1a8b9c66223f2c
Xiaoyi Liu <lxy878@gmail.com>
2020-08-19 10:14:47 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
 document.addEventListener('DOMContentLoaded', ()=>{
-  document.querySelector('div#modal')
+  document.querySelector('div#modal').className = 'hidden'
 })
 
 
