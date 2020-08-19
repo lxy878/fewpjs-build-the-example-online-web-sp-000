54c869de0685acc2f3160b6f7dd133b02eb1861a
Xiaoyi Liu <lxy878@gmail.com>
2020-08-19 10:14:02 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -3,7 +3,9 @@ const EMPTY_HEART = '♡'
 const FULL_HEART = '♥'
 
 // Your JavaScript code goes here!
-
+document.addEventListener('DOMContentLoaded', ()=>{
+  document.querySelector('div#modal')
+})
 
 
 
