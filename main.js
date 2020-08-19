38618c848730aa9c9a9a849809ca7cbe5b33ca28
Xiaoyi Liu <lxy878@gmail.com>
2020-08-19 11:15:00 +0000

    Automatically backed up by Learn

--- main.js
+++ main.js
@@ -34,7 +34,7 @@ document.addEventListener('DOMContentLoaded', ()=>{
 })
 
 const errorMessage = () => {
-
+  divModal.className = '';
 }
 
 //------------------------------------------------------------------------------
